c623873b6a537dc8517c4e3f295b7ea837ab9f0b
Tristan JUMEAUX <tristan.jumeaux@reseau.eseo.fr>
2021-04-09 23:03:56 +0200

    Get multiple references

--- get_reference.py
+++ get_reference.py
@@ -15,12 +15,18 @@ def find_closest_record(ref_to_check:str, df_wantlist:pd.DataFrame) -> pd.DataFr
     df_wantlist["comp"] = df_wantlist.apply(compare_names,args=("Artist","Title",ref_to_check),axis=1)
     return df_wantlist.loc[df_wantlist["comp"]>0.85]
 
+def forall_refs(refs):
+    df_result = pd.DataFrame(columns=["Reference","Artist","Title","Year","Label"])
+    for ref in refs:
+        df_result = df_result.append(find_closest_record(ref,df_wantlist).drop(columns="comp"))
+    return df_result
+
+
 if __name__ == "__main__":
 
-    ref_to_check = sys.argv[1]
+    refs_to_check = sys.argv[1:]
     df_wantlist = read_wantlist()
-    df_result = find_closest_record(ref_to_check,df_wantlist).drop(columns="comp")
-    
+    df_result=forall_refs(refs_to_check)
     if len(df_result)>0:
         print("Here are the refs you need : \n",df_result.head())
     else:
--- get_wantlist.py
+++ get_wantlist.py
@@ -18,11 +18,11 @@ def get_wantlist(user):
 def clean_names(wantlist):
     result = []
     for record in wantlist:
-        result.append([record.release.id,record.release.artists[0].name,record.release.title])
+        result.append([record.release.id,record.release.artists[0].name,record.release.title,record.release.year,record.release.labels[0].name])
     return result
 
 def write_csv(wantlist):
-    dfToFlush = pd.DataFrame.from_records(wantlist,columns=["Reference","Artist","Title"])
+    dfToFlush = pd.DataFrame.from_records(wantlist,columns=["Reference","Artist","Title","Year","Label"]).drop_duplicates()
     dfToFlush.to_csv("wantlist.csv",index=False,sep=";")
 
 if __name__ == "__main__":
@@ -31,4 +31,9 @@ if __name__ == "__main__":
 
     user = identify(token)
     wantlist = clean_names(get_wantlist(user))
-    write_csv(wantlist)
+    try:
+        write_csv(wantlist)
+        print("Congratulations ! Everything went perfectly. You're ready to find your references.")
+    except:
+        print("There was an error while writing the file :( \nPlease verify that you're allowed to write a file.")
+
